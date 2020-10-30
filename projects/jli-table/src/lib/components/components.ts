cff66b415c1e4bbc01b6104fd1257119de8fafbe
FLK <fkienzler@trapil.com>
2020-10-30 16:23:40 +0100

    export JliTableComponent

---
+++ projects/jli-table/src/lib/components/components.ts
@@ -0,0 +1,1 @@
+export * from './table/jli-table.component';
--- projects/jli-table/src/public_api.ts
+++ projects/jli-table/src/public_api.ts
@@ -1,5 +1,7 @@
 export * from './lib/jli-table.module';
 
+export * from './lib/components/components';
+
 export * from './lib/entities/entities';
 
 export * from './lib/pipes/values.pipe';
